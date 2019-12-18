a32e61a5c3988bcc1de38ef291c651fd5835da74
Unknown <mantykora@gmail.com>
2019-12-18 23:44:08 +0100

    add firebase, migrate to AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(),
         home: LoginView(),
-        initialRoute: 'login',
+        initialRoute: '/login',
         onGenerateRoute: Router.generateRoute,
       ),
     );
