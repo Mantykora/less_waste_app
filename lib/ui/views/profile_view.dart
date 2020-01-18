bee0c9cc9378cfec13f1e79f622e480eec4357d0
Unknown <mantykora@gmail.com>
2020-01-18 23:20:58 +0100

    add photo button to profile view

--- lib/ui/views/profile_view.dart
+++ lib/ui/views/profile_view.dart
@@ -21,65 +21,67 @@ class ProfileView extends StatelessWidget {
     return BaseView<ProfileModel>(
         builder: (context, model, child) => Scaffold(
                 body: CustomScrollView(slivers: <Widget>[
-                  SliverAppBar(
-                    stretch: true,
-                    onStretchTrigger: () {
-                      // Function callback for stretch
-                      return;
-                    },
-                    expandedHeight: 300.0,
-                    flexibleSpace: FlexibleSpaceBar(
-                      stretchModes: <StretchMode>[
-                        StretchMode.zoomBackground,
-                        StretchMode.blurBackground,
-                        StretchMode.fadeTitle,
-                      ],
-                      centerTitle: true,
-                      title: const Text('Flight Report'),
-                      background: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                              margin: EdgeInsets.only(top: 16.0),
-                              padding: EdgeInsets.only(left: 32.0, right: 32.0),
-                              child: ClipOval(
-                                child: Container(
-                                  color: Colors.red,
-                                  width: 150,
-                                  height: 150,
+              SliverAppBar(
+                stretch: true,
+                onStretchTrigger: () {
+                  // Function callback for stretch
+                  return;
+                },
+                expandedHeight: 300.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  stretchModes: <StretchMode>[
+                    StretchMode.zoomBackground,
+                    StretchMode.blurBackground,
+                    StretchMode.fadeTitle,
+                  ],
+                  centerTitle: true,
+                  title: Text(user.username),
+                  background: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.only(top: 16.0),
+                          padding: EdgeInsets.only(left: 32.0, right: 32.0),
+                          child: ClipOval(
+                            child: InkWell(
+                              customBorder: CircleBorder(),
+                              onTap: () {},
+                              child: Container(
+                                color: Colors.black38,
+                                width: 150,
+                                height: 150,
+                                child: Icon(
+                                  Icons.add_a_photo,
+                                  color: Colors.white,
+                                  size: 45,
                                 ),
-                              )
-                          ),
-                        ],
-                      ),
-                    ),
+                              ),
+                            ),
+                          )),
+                    ],
                   ),
+                ),
+              ),
               SliverList(
                 //itemExtent: 50.0,
-                delegate: SliverChildListDelegate(
-                    [
-                      ProfileTextField(nameController, "imię", false),
-                      ProfileTextField(lastNameController, "nazwisko", false),
-                      ProfileTextField(aboutMeController, "o mnie", true),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 54.0, right: 24.0, left: 24.0),
-                        child:  RaisedButton(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: new BorderRadius.circular(18.0),
-
-                              ),
-                              color: Theme.of(context).accentColor,
-                              child: Text("Aktualizuj"),
-                              onPressed: () {
-                             model.updateUserById(userId: user.id, name: nameController.text, lastName: lastNameController.text, description: aboutMeController.text );
-                          }
-
-                          ),
-
-                      )
-                    ]
-                ),
+                delegate: SliverChildListDelegate([
+                  ProfileTextField(nameController, "imię", false),
+                  ProfileTextField(lastNameController, "nazwisko", false),
+                  ProfileTextField(aboutMeController, "o mnie", true),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 54.0, right: 24.0, left: 24.0),
+                    child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(18.0),
+                        ),
+                        color: Theme.of(context).accentColor,
+                        child: Text("Aktualizuj"),
+                        onPressed: () {
+                          model.updateUserById(userId: user.id, name: nameController.text, lastName: lastNameController.text, description: aboutMeController.text);
+                        }),
+                  )
+                ]),
               )
             ])));
   }
