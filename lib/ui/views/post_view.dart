0c2ac4a1efb749991e619100e31b8a43c769349b
Unknown <mantykora@gmail.com>
2020-02-19 21:31:12 +0100

    add photo to post view

--- lib/ui/views/post_view.dart
+++ lib/ui/views/post_view.dart
@@ -71,17 +71,32 @@ class PostView extends StatelessWidget {
                   ),
                   Text(post.body),
                   // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/profile', arguments: post.userId);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/profile', arguments: post.userId);
+                      },
                       child: Row(
+                        mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          Text('autor: '),
-                          Text(user.username,
-                            style: TextStyle(fontWeight: FontWeight.w600),
+                          // Text('autor:'),
+                          user.profilePhotoUrl != null
+                              ? ClipOval(
+                            child: Container(
+                              child: Image.network(user.profilePhotoUrl),
+                              width: 40,
+                              height: 40,
+                            ),
+                          )
+                              : Container(),
+                          //TODO 1 inkwell na całość
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              user.username,
+                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+                            ),
                           ),
                         ],
                       ),
