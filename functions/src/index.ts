e8db0e11a38727604c18a48a0a6a8bf8e2e70fc6
Unknown <mantykora@gmail.com>
2020-04-09 20:25:50 +0200

    add onCreate Posts Cloud Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,40 @@
-import * as functions from 'firebase-functions';
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+
+
+export const createPo = functions.firestore.document('posts/{id}')
+      .onCreate((snap) => {
+
+      //const newValue = snap.data();
+
+      console.log('Message received');
+
+     const payload = {
+                      notification: {
+                        "title": "",
+                        "body": "Check Choice of the day.",
+                      }
+                    };
+
+      return admin.messaging().sendToTopic("News",payload)
+          .then(function(response){
+               console.log('Notification sent successfully:',response);
+          })
+          .catch(function(error){
+               console.log('Notification sent failed:',error);
+          });
+    });
+
