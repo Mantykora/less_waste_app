2e39272d95dbe6dbaf322ea4067d25219ba9812f
Unknown <mantykora@gmail.com>
2020-04-29 22:56:42 +0200

    add switch to handle subscribe/unsubscribe from topic

--- lib/core/viewmodels/settings_model.dart
+++ lib/core/viewmodels/settings_model.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:less_waste_app/core/enums/viewstate.dart';
 import 'package:less_waste_app/core/services/database.dart';
 
 import 'base_model.dart';
 
 class SettingsModel extends BaseModel {
   final DatabaseService database = DatabaseService();
+  FirebaseMessaging firebaseMessaging =  FirebaseMessaging();
 
-
+  void notificationSettings(bool value) {
+    if (value) {
+      firebaseMessaging.subscribeToTopic('News');
+      print('subscribe to topic News');
+      setState(ViewState.Idle);
+    } else {
+      firebaseMessaging.unsubscribeFromTopic("News");
+      print('unsubscribe from topic News');
+      setState(ViewState.Idle);
+    }
+  }
 
 }
--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
@@ -7,9 +8,26 @@ import 'package:less_waste_app/ui/views/base_view.dart';
 class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    FirebaseMessaging firebaseMessaging =  FirebaseMessaging();
+    bool isNotficationOn = true;
+
     return BaseView<SettingsModel>(
         builder: (context, model, child) => Scaffold(
           appBar: GradientAppBar(title: Text('Flutter'), gradient: LinearGradient(colors: [Colors.blue, Theme.of(context).primaryColor])),
+          body: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Switch(onChanged: (bool value) {
+                    isNotficationOn = value;
+                    model.notificationSettings(isNotficationOn);
+                  }, value: isNotficationOn,),
+                  Text('Powiadomienia w aplikacji')
+                ],
+              )
+            ],
+          ),
         ),
 
     );
